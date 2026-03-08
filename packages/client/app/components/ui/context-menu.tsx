e7cc3f89fdbf60745636d87561839e9276d70a29
Joel Fickson <joelngozo@gmail.com>
2026-03-08 23:20:14 +0100

    style(client): apply biome formatting to ui components

--- packages/client/app/components/ui/context-menu.tsx
+++ packages/client/app/components/ui/context-menu.tsx
@@ -1,8 +1,7 @@
-import * as React from "react";
+import { CheckIcon, ChevronRightIcon } from "lucide-react";
 import { ContextMenu as ContextMenuPrimitive } from "radix-ui";
-
+import type * as React from "react";
 import { cn } from "~/lib/utils";
-import { ChevronRightIcon, CheckIcon } from "lucide-react";
 
 function ContextMenu({
   ...props
